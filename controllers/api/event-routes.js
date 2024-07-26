da52c0ce10b14f86b7f81523af40912ba1e9905e
Erin DeVine <erinj291@gmail.com>
2024-07-25 21:51:13 -0400

    typo fixes

--- controllers/api/event-routes.js
+++ controllers/api/event-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const {Event, Location} = require('../../models');
-const withAuth = require('../../utils/auth');
+const { withAuth } = require('../../utils/auth');
 
 //GET all events
 router.get('/', async (req, res)=>{
