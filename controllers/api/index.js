0a835a90f4026534f48be50f2450efeac62d3347
Matt <70668167+MattJhet@users.noreply.github.com>
2024-07-25 20:49:09 -0400

    commit

--- controllers/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-//const userRoutes = require('./user-routes.js');
-const eventRoutes = require('./event-routes.js');
-
-//router.use('/users', userRoutes);
-router.use('/event', eventRoutes);
-
-module.exports = router;
