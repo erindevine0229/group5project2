406dbc48853959e13e06f2ad6b3c160a825dda26
Erin DeVine <erinj291@gmail.com>
2024-07-29 20:32:43 -0400

    login/signup bug is fixed. Now successfully using

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,47 +1,49 @@
 const router = require('express').Router();
-const {Event, Location} = require('../models');
+///////////////////////
+const { Event, Location } = require('../models');
 const { withAuth } = require('../utils/auth');
-
-//GET all events
-router.get('/', async (req, res)=>{
+///////////////////////////
+// router.get('/', async (req, res) => {
+//     res.render('homepage')
+// });
+/////////////////////////////////////////////////////////////
+router.get('/', async (req, res) => {
     try {
-        const 
-        eventData = await Event.findAll ({
+        const eventData = await Event.findAll({
             include: [{
                 model: Location,
                 attributes: ['zip_code'],
-            },
-        ],
+            }]
         });
 
         const events = eventData.map((event) =>
-            event.get({plain: true })
+            event.get({ plain: true })
         );
         res.render('homepage', {
-            events, 
+            events,
             loggedIn: req.session.loggedIn,
         });
-    } catch{
+    } catch {
         console.log(err);
         res.status(500).json(err);
     }
 });
 
 // GET one event
-router.get('/event/:event_id', withAuth, async (req, res) =>{
+router.get('/event/:event_id', withAuth, async (req, res) => {
     try {
-      const eventData = await Event.findByPk(req.params.event_id, {
-        include: [{
-            model: Location,
-            attributes:[
-                'location_id', 'zip_code',
+        const eventData = await Event.findByPk(req.params.id, {
+            include: [{
+                model: Location,
+                attributes: [
+                    'location_id', 'location_name', 'zip_code',
+                ],
+            },
             ],
-        },
-    ],
-      });
-      
-      const event = eventData.get({ plain: true });
-      res.render('event', {event, loggedIn: req.session.loggedIn});
+        });
+
+        const event = eventData.get({ plain: true });
+        res.render('event', { event, loggedIn: req.session.loggedIn });
     } catch {
         console.log(err);
         res.status(500).json(err);
@@ -51,21 +53,26 @@ router.get('/event/:event_id', withAuth, async (req, res) =>{
 router.get('/location/:id', withAuth, async (req, res) => {
     try {
         const dbLocationData = await Location.findByPk(req.params.id);
+
         const locations = dbLocationData.get({ plain: true });
+
         res.render('locations', { locations, loggedIn: req.session.loggedIn });
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
     }
 });
+
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
         res.redirect('/');
         return;
     }
+
     res.render('login');
 });
 
+//////////////////////////////////////////////////////////
 
 router.get('/signup', async (req, res) => {
     res.render('signUp')
